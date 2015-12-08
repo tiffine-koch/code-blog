6eb826aa86596872a7dadc69a64494c657b190b2
Apple <tiffine.koch@gmail.com>
2015-12-08 12:44:20 -0800

    contains class notes from Ivan and Jon

--- blog_js/utls.js
+++ blog_js/utls.js
@@ -1,4 +1,45 @@
+//notes from 12/8/15
 //Ajax call to grab template
 $.get('dialog/speech1.txt'.function(data) {
   console.log(data);
+}).done(function() {
+  console.log('I should come second!');
+  console.log('I should come third!');
 });
+
+//or wrap into $.get but could have user issues$.get('dialog/speech1.txt'.function(data) {
+$.get('dialog/speech1.txt'.function(data) {
+  console.log(data);
+  console.log('I should come second!');
+  console.log('I should come third!');
+});
+//test for ajax calls
+$.get('dialog/speech1.txt'.function(data) {
+  console.log('Im the data!');
+  console.log(data);
+}).always(function() {
+  console.log('I should come always!');
+}).fail(function() {
+  console.log('I should come if fail!');
+});
+
+//condensed version -- but anonymous function
+$.get('dialog/speech1.txt').done(function() {
+  console.log('I should come second!');
+});
+  console.log('I should come third!');
+
+//name function - no anomymous function
+demo = {};
+demo.getData = (function (data) {
+  console.log(data);
+};
+demo.getDone = function () {
+  console.log('I should come two');
+}
+demo.getDoneR = function () {
+  console.log('I should come third!');
+}
+$.get('dialog/speech1.txt', demo.getData)
+  .done(demo.getDone)
+  .done(demo.getDoneR);
