dc51f0b36e662074500139339c51d3c9f6e7c1f4
Apple <tiffine.koch@gmail.com>
2015-12-08 09:35:15 -0800

    updates

--- blog_js/utls.js
+++ blog_js/utls.js
@@ -1,23 +1,4 @@
-//handlbars
-//code structure from javascriptissexy.com
-$(function () {
-  // get template script
-var templateScript = $("#article-template").html();
-  // compile template
-var compileTemplate = Handlebars.compile(templateScript);
- // define object
- //will adapt this to form and write in markdown
-var newArticle = new Article(content) {
-  title: $('#article-title').val(),
-  category: $('#article-category').val(),
-  author: $('#article-author').val(),
-  authorUr;: $('#article-authorUrl').val(),
-  body: $('#article-body').val(),
-  publishedOn: $('#article-publishedOn').val(),
-  };
-// pass formData to template
-var compiledHtml = compileTemplate(content);
-// Add the compiled html to the page
-  $('.content-placeholder').newArticle.toHTML();
-//append - write code
+//Ajax call to grab template
+$.get('dialog/speech1.txt'.function(data) {
+  console.log(data);
 });
